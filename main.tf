a6705a5831934914c05586f0ee4af7ff79d1c43c
Shmuel Kaufmann <shmuel.kaufmann1@gmail.com>
2023-06-06 11:57:35 +0300

    main.tf changed for s3 + ec2

--- main.tf
+++ main.tf
@@ -6,15 +6,15 @@ resource "aws_instance" "example" {
     ami = "ami-0aa2b7722dc1b5612"
     instance_type = "t2.micro"
     tags = {
-        Name = "Terraform-instance"
+        Name = "Terraform-instance-2"
     }
 }
 
 resource "aws_s3_bucket" "mys3" {
-    bucket = "terraform-bucket-shmuel-public"
+    bucket = "terraform-bucket-shmuel-public-2"
 }
 
-resource "aws_s3_bucket_public_access_block" "mys3" {
+resource "aws_s3_bucket_public_access_block-2" "mys3" {
     bucket = aws_s3_bucket.mys3.id
     block_public_acls = false
     block_public_policy = false
